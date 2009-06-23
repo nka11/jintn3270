90196f2c630b8307fbb101211f56cb0b8d4e2684
bryanvarner <bryanvarner@d6bd4e80-8031-4e1d-bd39-93c407d940fc>
2009-06-23 18:45:57 +0000

    More cleanup / documentation
    git-svn-id: https://jintn3270.svn.sourceforge.net/svnroot/jintn3270/trunk@32 d6bd4e80-8031-4e1d-bd39-93c407d940fc

--- src/com/sf/jintn3270/TerminalModel.java
+++ src/com/sf/jintn3270/TerminalModel.java
@@ -3,7 +3,8 @@ package com.sf.jintn3270;
 /**
  * A TerminalModel is where telnet stream data goes to be rendered by a view.
  * A model maintains a list of listeners, which are notified when changes to the 
- * buffer occur.
+ * buffer occur. It also tracks a CursorPosition, which is used when printing
+ * data into the buffer.
  */
 public abstract class TerminalModel {
 	TerminalCharacter[][] buffer;
@@ -12,12 +13,21 @@ public abstract class TerminalModel {
 	
 	CursorPosition cursor;
 	
+	/**
+	 * Constructs a TerminalModel with the given number of rows and cols. 
+	 * The model will use the given CharacterFactory to render print() 
+	 * invocations into the Buffer.
+	 */
 	protected TerminalModel(int rows, int cols, CharacterFactory charFact) {
 		this.charFact = charFact;
 		cursor = new CursorPosition();
 		initializeBuffer(rows, cols);
 	}
 	
+	/**
+	 * Initializes the buffer by allocate a new buffer array, then filling
+	 * the array with the character mapped to byte 0x00
+	 */
 	protected void initializeBuffer(int rows, int cols) {
 		buffer = new TerminalCharacter[rows][cols];
 		byte b = 0;
@@ -29,64 +39,92 @@ public abstract class TerminalModel {
 		// TODO: Fire Buffer Changed
 	}
 	
+	/**
+	 * Erase the last character printed. This moved the cursor to the left, 
+	 * THEN sets the character to 0x00.
+	 * 
+	 * This would be like a 'backspace'.
+	 */
 	public void eraseChar() {
 		cursor.left();
-		buffer[cursor.row][cursor.column] = charFact.get((byte)0);
+		buffer[cursor.row()][cursor.column()] = charFact.get((byte)0);
 		// TODO: Fire Buffer Changed
 	}
 	
+	/**
+	 * Erases the last line printed (Based on cursor position). This sets the 
+	 * entire line to 0x00, then moves the cursor to column 0.
+	 */
 	public void eraseLine() {
-		for (int col = 0; col < buffer[cursor.row].length; col++) {
-			buffer[cursor.row][col] = charFact.get((byte)0);
+		for (int col = 0; col < buffer[cursor.row()].length; col++) {
+			buffer[cursor.row()][col] = charFact.get((byte)0);
 		}
-		cursor.moveTo(cursor.row, 0);
+		cursor.moveTo(cursor.row(), 0);
 		// TODO: Fire Buffer Changed
 	}
 	
-	
-	public void print(byte[] bytes) {
-		print(bytes, 0, bytes.length);
-		// TODO: Fire buffer changed.
+	/**
+	 * Gets the current CursorPosition used by this TerminalModel.
+	 */
+	public CursorPosition getCursor() {
+		return cursor;
 	}
 	
-	
-	public void printChar(byte b) {
-		print(b);
-		// TODO: Fire buffer changed.
-	}
-	
-	
-	private void print(byte b) {
-		TerminalCharacter ch = charFact.get(b);
+	/**
+	 * Method that sets the buffer data at the current cursor position, then
+	 * advances the cursor position (by calling cursor.right()) one position.
+	 */
+	protected void print(TerminalCharacter ch) {
 		boolean display = true;
 		if (ch.getDisplay() == '\n') {
 			cursor.down();
 			display = false;
 		}
 		if (ch.getDisplay() == '\r') {
-			cursor.column = 0;
+			cursor.moveTo(cursor.row(), 0);
 			display = false;
 		}
 		
 		if (display) {
-			buffer[cursor.row][cursor.column] = ch;
+			buffer[cursor.row()][cursor.column()] = ch;
 			cursor.right();
 		}
 	}
 	
+	/**
+	 * Prints the given byte at the current cursor location.
+	 */
+	public void print(byte b) {
+		print(charFact.get(b));
+		// TODO: Fire buffer changed.
+	}
 	
+	/**
+	 * Prints the given array of bytes, starting at offset, up to length, at 
+	 * the current cursor location.
+	 */
 	public void print(byte[] bytes, int offset, int length) {
 		for (int pos = offset; pos < (offset + length); pos++) {
-			print(bytes[pos]);
+			print(charFact.get(bytes[pos]));
 		}
 		// TODO: Fire buffer changed.
 	}
 	
+	/**
+	 * Prints the given array of bytes at the current cursor location.
+	 */
+	public void print(byte[] bytes) {
+		print(bytes, 0, bytes.length);
+		// TODO: Fire buffer changed.
+	}
 	
 	
+	/**
+	 * CursorPosition is closely tied to the buffer.
+	 */
 	public class CursorPosition {
-		int row;
-		int column;
+		private int row;
+		private int column;
 		
 		public CursorPosition() {
 			this.row = 0;
@@ -98,12 +136,19 @@ public abstract class TerminalModel {
 			this.column = column;
 		}
 		
-		public int getRow() {
+		public int row() {
 			return row;
 		}
 		
-		public int getColumn() {
+		public int column() {
 			return column;
+		}
+		
+		/**
+		 * Gets the position as if it were being used to index a linear buffer.
+		 */
+		public int getPosition() {
+			return (row * buffer[0].length) + column;
 		}
 		
 		
