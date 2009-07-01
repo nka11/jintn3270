967a3b5d84b6d5c425f107d3ebd2662dbb89619e
bryanvarner <bryanvarner@d6bd4e80-8031-4e1d-bd39-93c407d940fc>
2009-07-01 20:00:54 +0000

    setting up a test client for tn3270e.
    git-svn-id: https://jintn3270.svn.sourceforge.net/svnroot/jintn3270/trunk@73 d6bd4e80-8031-4e1d-bd39-93c407d940fc

--- src/com/sf/jintn3270/tn3279/TestClient.java
+++ src/com/sf/jintn3270/tn3279/TestClient.java
@@ -12,7 +12,6 @@ public class TestClient {
 			new TelnetClient(args[0], Integer.parseInt(args[1]), 
 			Boolean.valueOf(args[2]).booleanValue(), 
 			new TerminalModel3279(TerminalModel3279.TermType3279.MODEL4));
-		client.addOption(new TerminalType());
 		client.start();
 	}
 }
