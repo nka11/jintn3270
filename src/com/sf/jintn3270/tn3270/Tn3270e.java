fa281ae1ba999bfed2973f1d22230f99cb092e66
bryanvarner <bryanvarner@d6bd4e80-8031-4e1d-bd39-93c407d940fc>
2009-07-21 19:34:10 +0000

    Cleanup and documentation
    git-svn-id: https://jintn3270.svn.sourceforge.net/svnroot/jintn3270/trunk@90 d6bd4e80-8031-4e1d-bd39-93c407d940fc

--- src/com/sf/jintn3270/tn3270/Tn3270e.java
+++ src/com/sf/jintn3270/tn3270/Tn3270e.java
@@ -14,23 +14,22 @@ import java.io.IOException;
 
 /**
  * TODO: Allow specifying a device name, properly handle REJECT during the CONNECT phase.
+ *
+ * This implementation is currently very um... stub-ish.
+ *
+ * We currently only support no-function (Basic TN3270e) connections without 
+ * named devices (generic pool devices only), and we do not (yet) perform the 
+ * equivalent of a 3270 erase / reset when switching data modes.
  */
 public class Tn3270e extends Option implements TelnetConstants {
-	public static final int MODE_3270 = 0;
-	public static final int MODE_NVT = 5;
-
-	EndOfRecord eor;
-	Binary binary;
-	
-	String deviceType;
-	String deviceName;
-	
-	private int dataMode;
-	
-	ArrayList<Function> requestedFunctions;
-	
 	public static final short TN3270E = 40;
 	
+	public static final int MODE_3270 = 0;
+	public static final int MODE_NVT = 5;
+	
+	/**
+	 * Enum used to determine Command. Ordinal values match RFC constants
+	 */
 	public enum Command {
 		ASSOCIATE, 
 		CONNECT, 
@@ -43,6 +42,9 @@ public class Tn3270e extends Option implements TelnetConstants {
 		SEND
 	};
 	
+	/**
+	 * Enum used to determine Reason. Ordinal Values match RFC constants.
+	 */
 	public enum Reason {
 		CONN_PARTNER,
 		DEVICE_IN_USE,
@@ -54,6 +56,9 @@ public class Tn3270e extends Option implements TelnetConstants {
 		UNSUPPORTED_REQ
 	};
 	
+	/**
+	 * Enum used for 3270 Functions. Ordinal Values match RFC constants.
+	 */
 	public enum Function {
 		BIND_IMAGE,
 		DATA_STREAM_CTL,
@@ -63,6 +68,24 @@ public class Tn3270e extends Option implements TelnetConstants {
 	};
 	
 	
+	
+	/* -- Members -- */
+	
+	private EndOfRecord eor;
+	private Binary binary;
+	
+	private String deviceType;
+	private String deviceName;
+	
+	private int dataMode;
+	
+	ArrayList<Function> requestedFunctions;
+	
+	
+	/**
+	 * Construct a new Tn3270e that uses the given EOR and Binary options for
+	 * processing 3270 data streams.
+	 */
 	public Tn3270e(EndOfRecord eor, Binary b) {
 		super();
 		deviceType = "";
@@ -75,20 +98,34 @@ public class Tn3270e extends Option implements TelnetConstants {
 		dataMode = -1;
 	}
 	
+	
 	public String getName() {
 		return "Tn3270e";
 	}
+	
 	
 	public short getCode() {
 		return TN3270E;
 	}
 	
-	
+	/**
+	 * Do not initiate.
+	 */
 	public void initiate(TelnetClient client) {
 	}
 	
+	/**
+	 * If we've negotiated our connection and entered a dataMode, 
+	 * then parse the incoming data as a frame.
+	 * A frame consists of a 5-byte header and IAC EOR.
+	 * 
+	 * Without IAC EOR, we will not parse the incoming bytes.
+	 *
+	 * Once an IAC EOR is found, we read the header, then pass the data
+	 * to the proper destination -- either TerminalModel for NVT, or the Binary
+	 * option to be parsed by the 3270 parser.
+	 */
 	public int consumeIncoming(short[] incoming, TelnetClient client) {
-		System.out.println("incoming length: " + incoming.length);
 		// If the binary option is enabled, we've negotiated the stream
 		// successfully, and need to look for 3270 frames.
 		if (dataMode != -1) {
@@ -106,8 +143,6 @@ public class Tn3270e extends Option implements TelnetConstants {
 				
 				// If we find an EOR record, then we process the frame.
 				if (found) {
-					System.out.println("Found IAC, EOR at: " + dataEnd);
-					
 					// Parse the 5-byte header.
 					short dataType = incoming[0];
 					short requestFlag = incoming[1];
@@ -120,7 +155,6 @@ public class Tn3270e extends Option implements TelnetConstants {
 						// TODO: Switch data modes from 3270 to NVT or 
 						// from NVT to 3270
 					}
-					System.out.println("Data Length: " + (incoming.length - 7));
 					short[] dataFrame = new short[incoming.length - 7];
 					System.arraycopy(incoming, 5, dataFrame, 0, dataFrame.length);
 					
@@ -134,15 +168,17 @@ public class Tn3270e extends Option implements TelnetConstants {
 						client.getTerminalModel().print(dataFrame);
 					}
 					
-					System.out.println("Consumed 3270 data: " + (dataEnd + 1) + " bytes");
 					return dataEnd + 1;
 				}
 			}
 		}
-		// Otherwise, there's not enough data to process this as a frame.
+		// No Negotiated data mode, not enough bytes in the buffer, or no IAC EOR found.
 		return 0;
 	}
 	
+	/**
+	 * Handles sub-commands for option negotiation of Tn3270e.
+	 */
 	public int consumeIncomingSubcommand(short[] incoming, TelnetClient client) {
 		int length = 0;
 		for (int i = 0; i < incoming.length - 1; i++) {
@@ -150,14 +186,11 @@ public class Tn3270e extends Option implements TelnetConstants {
 				length = i + 2;
 			}
 		}
-		System.out.println("Subcommand is " + length + " bytes long.");
 		if (length > 0) {
-			System.out.println("Subcommand: " + resolveValue(incoming[3], Command.class) + " " + resolveValue(incoming[4], Command.class));
 			if (incoming[3] == Command.SEND.ordinal() && 
 			    incoming[4] == Command.DEVICE_TYPE.ordinal()) 
 			{
 				try {
-					System.out.println("Sending DEVICE_TYPE REQUEST");
 					out.write(new short[] {IAC, SB, getCode(), (short)Command.DEVICE_TYPE.ordinal(), (short)Command.REQUEST.ordinal()});
 					out.write(((client.getTerminalModel().getModelName())[0]).getBytes("ASCII"));
 					out.write(new short[] {IAC, SE});
@@ -183,8 +216,6 @@ public class Tn3270e extends Option implements TelnetConstants {
 				}
 				deviceName = buf.toString();
 				
-				System.out.println("Received type: " + deviceType + " name: " + deviceName);
-				
 				// Send a request with the list of functions we intend to support.
 				// For now, we'll NOT send any, and just implement "Basic TN3270E".
 				//requestedFunctions.add(Function.BIND_IMAGE);
@@ -207,10 +238,12 @@ public class Tn3270e extends Option implements TelnetConstants {
 				for (int i = 5; i < length - 2; i++) {
 					proposed.add((Function)(resolveValue(incoming[i], Function.class)));
 				}
+				/*
 				for (Function f : proposed) {
 					System.out.println("  server proposed: " + f);
 				}
 				System.out.println("Setting proposed as my list");
+				*/
 				requestedFunctions = proposed;
 				requestFunctions();
 			} else if (incoming[3] == Command.FUNCTIONS.ordinal() &&
@@ -226,10 +259,8 @@ public class Tn3270e extends Option implements TelnetConstants {
 					match = requestedFunctions.contains(proposed.get(i++));
 				}
 				if (match) {
-					System.out.println("FUNCTIONS IS matched!");
 					dataMode = MODE_3270;
 				} else {
-					System.out.println("FUNCTIONS IS did NOT match!");
 					requestedFunctions = proposed;
 					requestFunctions();
 				}
