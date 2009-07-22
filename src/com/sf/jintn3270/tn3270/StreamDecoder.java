a368b4b1e0cfb957b87aa4972903cea7b79614fe
bryanvarner <bryanvarner@d6bd4e80-8031-4e1d-bd39-93c407d940fc>
2009-07-22 14:02:44 +0000

    Beginning of stream parsing. I believe i need to flesh out the model before I continue to pursue this.
    git-svn-id: https://jintn3270.svn.sourceforge.net/svnroot/jintn3270/trunk@92 d6bd4e80-8031-4e1d-bd39-93c407d940fc

--- src/com/sf/jintn3270/tn3270/StreamDecoder.java
+++ src/com/sf/jintn3270/tn3270/StreamDecoder.java
@@ -11,7 +11,22 @@ import java.io.OutputStream;
  * TerminalModel3278 it's constructed to control.
  */
 public class StreamDecoder extends UByteOutputStream {
+	/* 3270 Commands */
+	private static final int READ_MODIFIED_ALL = 0x6e;
+	private static final int ERASE_ALL_UNPROTECTED = 0x6f;
+	private static final int ERASE_WRITE_ALTERNATE = 0x7e;
+	private static final int WRITE = 0xf1;
+	private static final int READ_BUFFER = 0xf2;
+	private static final int WRITE_STRUCTURED_FIELD = 0xf3;
+	private static final int ERASE_WRITE = 0xf5;
+	private static final int READ_MODIFIED = 0xf6;
+	
+	
+	
 	private TerminalModel3278 terminal;
+	
+	private int currentCommand = 0;
+	
 	
 	public StreamDecoder(TerminalModel3278 terminal) {
 		super(null);
@@ -28,14 +43,39 @@ public class StreamDecoder extends UByteOutputStream {
 	}
 	
 	public void write(byte[] b, int off, int len) throws IOException {
+		int nextByte = 0;
 		System.out.println("StreamParser received " + len  + " bytes");
+		
+		if (currentCommand == 0) {
+			currentCommand = b[nextByte++] & 0xff;
+		}
+		
+		System.out.println("Processing data from Command: " + Integer.toHexString(currentCommand));
+		switch (currentCommand) {
+			case READ_MODIFIED_ALL:
+				break;
+			case ERASE_ALL_UNPROTECTED:
+				break;
+			case ERASE_WRITE_ALTERNATE:
+				break;
+			case WRITE:
+				break;
+			case READ_BUFFER:
+				break;
+			case WRITE_STRUCTURED_FIELD:
+				break;
+			case ERASE_WRITE:
+				break;
+			case READ_MODIFIED:
+				break;
+		}
 	}
 	
 	public void write(byte[] b) throws IOException {
-		System.out.println("StreamParser received " + b.length + " bytes");
+		write(b, 0, b.length);
 	}
 	
 	public void write(int b) throws IOException {
-		System.out.println("StreamParser received byte");
+		write(new byte[] {(byte)b});
 	}
 }
